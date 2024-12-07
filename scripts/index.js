bb472fde0b5fde634dd29b0d41da45fa383427f9
Alir3za <alireza2212kazemi@gmail.com>
2024-12-07 13:49:50 +0330

    form login button is done working with one special user

--- scripts/index.js
+++ scripts/index.js
@@ -1,14 +1,48 @@
-'use strict'
+"use strict";
 
-let form = document.getElementById('form');
-console.log(form);
+let form = document.getElementById("form");
 
-document.addEventListener('submit', (event)=>{
-  event.preventDefault();
-  let formData = new FormData(document.getElementById("form"));
-  let userObj = Object.fromEntries(formData)
-  
-  let jsonObj = JSON.stringify(userObj);
-  localStorage.setItem("first-form", jsonObj);
-  window.location.href = 'second.html'
-})
+let passReg = new RegExp('^((?=.*[\d])(?=.*[a-z])(?=.*[A-Z])|(?=.*[a-z])(?=.*[A-Z])(?=.*[^\w\d\s])|(?=.*[\d])(?=.*[A-Z])(?=.*[^\w\d\s])|(?=.*[\d])(?=.*[a-z])(?=.*[^\w\d\s])).{7,30}$');
+
+let emailReg = new RegExp(
+	"[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?"
+);
+
+document.addEventListener("submit", (event) => {
+	event.preventDefault();
+
+	let validatePass = passReg.test(passInput.value);
+	let validateEmail = emailReg.test(emailInput.value);
+
+	if (!validateEmail) {
+		let emailError = document.createElement("span");
+		emailError.classList += "error";
+		emailError.textContent = "wrong Gmail format";
+		emailInput.after(emailError);
+
+		setTimeout(() => {
+			emailError.remove();
+		}, 3000);
+	}
+
+	if (!validatePass) {
+		let passError = document.createElement("span");
+		passError.classList += "error";
+		passError.textContent = "wrong Password format";
+		passInput.after(passError);
+
+		setTimeout(() => {
+			passError.remove();
+		}, 3000);
+	}
+
+	if (validateEmail && validatePass) {
+		let formData = new FormData(form);
+		let emailInput = document.getElementById("emailInput");
+		formData.append("email", emailInput.value);
+		let userObj = Object.fromEntries(formData);
+		let jsonObj = JSON.stringify(userObj);
+		localStorage.setItem("first-form", jsonObj);
+		window.location.href = "second.html";
+	}
+});
--- scripts/second.js
+++ scripts/second.js
@@ -4,7 +4,10 @@ let getData = localStorage.getItem("first-form");
 
 let dataObj = JSON.parse(getData);
 
-if (dataObj.username == 'alireza') {
+console.log(dataObj);
+
+
+if (dataObj.username.toLowerCase() == 'alireza' && dataObj.username.toLowerCase() == 'kazemi' ) {
   for(let key in dataObj){
     let html = `
     <div class="data">
@@ -23,5 +26,21 @@ if (dataObj.username == 'alireza') {
   `;
 
   let show = document.getElementById('data');
-  show.innerHTML = html
+  show.innerHTML = html;
+  let counter = document.createElement('span');
+  let i = 3;
+  counter.innerText = i;
+  show.append(counter);
+  setInterval(()=>{
+    counter.innerText = i--;
+  },1000)
+
+  setTimeout(() => {
+    window.location.href = 'index.html'
+  }, 4000);
+  let confirmButton = document.getElementById("confim_button");
+  confirmButton.innerText = 'Back to Login';
+  confirmButton.addEventListener("click", () => {
+		window.location.href = "index.html";
+  });
 }
